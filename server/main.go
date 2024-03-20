0feee9521aabebc69543558fd8cd57081f5888c5
MICHAELKITH <michaelkithinji27@gmail.com>
2024-03-20 15:04:28 +0300

    Complete backend golang app

--- server/main.go
+++ server/main.go
@@ -19,7 +19,7 @@ func main() {
 
 	app := fiber.New()
 
-	todo := []Todo{}
+	todos:= []Todo{}
 
 	app.Get("/pro", func(c *fiber.Ctx) error {
 		return c.SendString("Ok pro mike ")
@@ -31,11 +31,35 @@ func main() {
 		 if err := c.BodyParser(todo); err != nil{
 			return nil
 		 }
-		 todo.ID =len(todos)+1;
+		 todo.ID =len(todos) +1;
 
 		 todos = append(todos, *todo)
 
 		 return c.JSON(todos)
+	});
+
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id");
+
+		if err != nil{
+			return c.Status(401).SendString("Invalid id")
+
+		}
+
+		for i, t := range todos{
+			if t.ID == id {
+				todos[i].Done = true
+
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
